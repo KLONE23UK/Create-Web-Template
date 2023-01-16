6002fa44b2b5f808c14e7ec7de5aaf7ab2b306e6
Ryan Gilluley <stillactivity@protonmail.com>
2023-01-16 23:03:20 +0000

    Add overwrite options, add banner and colour

--- create_web_template.sh
+++ create_web_template.sh
@@ -3,14 +3,32 @@
 # create_web_template
 # By; Ryan Gilluley
 
+blue="\e[34m"
+endColour="\e[0m"
+banner="${blue}*********************************\n*${endColour}\tcreate_web_template\t${blue}*\n*********************************${endColour}\n"
+
 setup() {
     # Setup Step
     declare -A bootstrap=( [bootstrapCSS]="<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" [bootstrapJS]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js\" integrity=\"sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V\" crossorigin=\"anonymous\"></script>" [bootstrapJQ]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>" )
 
+    echo -e $banner
     echo "What will this project be called?"
     read projectName
 
-    makeFiles $bootstrap
+    if [ -d "$projectName" ]; then
+        echo "A directory with the same name already exists. Would you like to overwrite it? (y/n)"
+        read overwrite
+        if [ $overwrite = 'y' ]; then
+            echo "Overwriting..."
+            rm -rf $projectName
+            makeFiles $projectName
+        else
+            echo "exiting"
+            exit
+        fi
+    else
+        makeFiles $bootstrap
+    fi
 }
 
 makeFiles () {
@@ -52,6 +70,7 @@ EOF
 
 setup
 
-echo -e "Folder and files created;"
-find $projectName
+echo -e "\nFolder and files created;"
+structure=`find $projectName`
+echo -e "${blue}$structure${endColour}"
  
