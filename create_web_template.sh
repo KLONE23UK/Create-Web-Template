bd3d753f97fe4019fc169949700dcf792f02861e
Ryan Gilluley <stillactivity@protonmail.com>
2023-01-17 17:21:41 +0000

    Setup template option, add getopts options and seperate into more functions

--- create_web_template.sh
+++ create_web_template.sh
@@ -6,29 +6,50 @@
 blue="\e[34m"
 endColour="\e[0m"
 banner="${blue}*********************************\n*${endColour}\tcreate_web_template\t${blue}*\n*********************************${endColour}\n"
+declare -A bootstrap=( [bootstrapCSS]="<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" [bootstrapJS]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js\" integrity=\"sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V\" crossorigin=\"anonymous\"></script>" [bootstrapJQ]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>" )
 
-setup() {
-    # Setup Step
-    declare -A bootstrap=( [bootstrapCSS]="<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" [bootstrapJS]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js\" integrity=\"sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V\" crossorigin=\"anonymous\"></script>" [bootstrapJQ]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>" )
-
-    echo -e $banner
-    echo "What will this project be called?"
-    read projectName
-
+checkDirectory() {
     if [ -d "$projectName" ]; then
         echo "A directory with the same name already exists. Would you like to overwrite it? (y/n)"
         read overwrite
         if [ $overwrite = 'y' ]; then
             echo "Overwriting..."
             rm -rf $projectName
-            makeFiles $projectName
+            makeFiles $projectName $template
         else
             echo "exiting"
-            exit
+            exit 1
         fi
     else
         makeFiles $bootstrap
     fi
+}
+
+template() {
+    if [[ $template = "" ]]; then
+        echo 'Select a template from the list below (1/2...)
+1. Blank'
+        while true; do
+            read template
+            case $template in
+                1)
+                    checkDirectory $projectName
+                    break
+                ;;
+                *)
+                    echo "invalid argument - please select use a number to select which template you would like to use (1,2...)"
+                ;;
+            esac
+        done
+    else
+        checkDirectory $projectName
+    fi
+}
+
+setup() {
+    echo "What will this project be called?"
+    read projectName
+    template $projectName
 }
 
 makeFiles () {
@@ -43,12 +64,14 @@ makeFiles () {
     touch $projectName/js/index.js
     touch $projectName/index.htm
 
-    htmlTemplate $bootstrap
+    htmlTemplate $bootstrap $template
 }
 
 htmlTemplate () {
     # Add boilerplate to HTML file
-    cat > $projectName/index.htm << EOF
+    case $template in
+        1)
+            cat > $projectName/index.htm << EOF
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,24 +89,34 @@ htmlTemplate () {
 </body>
 </html>
 EOF
+        ;;
+    esac
 }
 
-while getopts "n:" options; do
+echo -e $banner
+
+while getopts "n:t:" options; do
     case "${options}" in
-        n)
+        n )
             projectName=${OPTARG}
         ;;
-        :)
-            echo "error: -${OPTARG} requires an argument."
+        t )
+            template=${OPTARG}
         ;;
+        : )
+            echo -e "*Cannot set option: ${OPTARG} : Starting normally without options.*\n"
+        ;;
+        * )
+            echo -e "*invalid command: ${OPTARG} : Starting normally without options.*\n"
     esac
 done
 
 if [[ $projectName = "" ]]; then
     setup
 else
-    makeFiles $projectName
+    template $projectName
 fi
+
 
 echo -e "\nDirectories and files created;"
 structure=`find $projectName`
