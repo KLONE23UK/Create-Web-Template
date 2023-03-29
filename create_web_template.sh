2f8481a0541b525fe7a72e2ac70f57d40f05c344
Ryan Gilluley <stillactivity@protonmail.com>
2023-03-29 21:18:49 +0100

    Fix multi-word project name

--- create_web_template.sh
+++ create_web_template.sh
@@ -5,7 +5,7 @@
 
 blue="\e[34m"
 endColour="\e[0m"
-banner="${blue}*********************************\n*${endColour}\tcreate_web_template\t${blue}*\n*********************************${endColour}\n"
+banner="${blue}―――――――――――――――――――――――――――――――――\n|${endColour}\tcreate_web_template\t${blue}|\n―――――――――――――――――――――――――――――――――${endColour}\n"
 declare -A bootstrap=( [bootstrapCSS]="<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\" integrity=\"sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65\" crossorigin=\"anonymous\">" [bootstrapJS]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js\" integrity=\"sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V\" crossorigin=\"anonymous\"></script>" [bootstrapJQ]="<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4\" crossorigin=\"anonymous\"></script>" )
 
 checkDirectory() {
@@ -15,7 +15,7 @@ checkDirectory() {
         if [ $overwrite = 'y' ]; then
             echo "Overwriting..."
             rm -rf $projectName
-            makeFiles $projectName $template
+            makeFiles "$projectName" $template
         else
             echo "exiting"
             exit 1
@@ -28,17 +28,17 @@ checkDirectory() {
 template() {
     if [[ $template = "" ]]; then
         echo 'Select a template from the list below (1/2...)
-1. Blank
-2. Simple'
+    1. Blank
+    2. Simple'
         while true; do
             read template
             case $template in
                 1)
-                    checkDirectory $projectName
+                    checkDirectory "$projectName"
                     break
                 ;;
                 2)
-                    checkDirectory $projectName
+                    checkDirectory "$projectName"
                     break
                 ;;
                 *)
@@ -54,20 +54,21 @@ template() {
 setup() {
     echo "What will this project be called?"
     read projectName
+    echo $projectName
     template $projectName
 }
 
 makeFiles () {
     # Make Folders
-    mkdir $projectName
-    mkdir $projectName/css
-    mkdir $projectName/img
-    mkdir $projectName/js
+    mkdir "$projectName"
+    mkdir "$projectName/css"
+    mkdir "$projectName/img"
+    mkdir "$projectName/js"
 
     # Make files
-    touch $projectName/css/style.css
-    touch $projectName/js/index.js
-    touch $projectName/index.htm
+    touch "$projectName/css/style.css"
+    touch "$projectName/js/index.js"
+    touch "$projectName/index.htm"
 
     htmlTemplate $bootstrap $template
 }
@@ -76,7 +77,7 @@ htmlTemplate () {
     # Add boilerplate to HTML file
     case $template in
         1)
-            cat > $projectName/index.htm << EOF
+            cat > "$projectName/index.htm" << EOF
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -96,7 +97,7 @@ htmlTemplate () {
 EOF
         ;;
         2)
-            cat > $projectName/index.htm << EOF
+            cat > "$projectName/index.htm" << EOF
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -104,7 +105,7 @@ EOF
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     ${bootstrap[bootstrapCSS]}
     <title>$projectName</title>
 </head>
@@ -144,10 +145,10 @@ echo -e $banner
 while getopts "n:t:" options; do
     case "${options}" in
         n )
-            projectName=${OPTARG}
+            projectName="${OPTARG}"
         ;;
         t )
-            template=${OPTARG}
+            template="${OPTARG}"
         ;;
         : )
             echo -e "*Cannot set option: ${OPTARG} : Starting normally without options.*\n"
@@ -165,5 +166,5 @@ fi
 
 
 echo -e "\nDirectories and files created;"
-structure=`find $projectName`
+structure=`find "$projectName"`
 echo -e "${blue}$structure${endColour}"
